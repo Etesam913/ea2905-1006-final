379cf24fdb1ad1819c9dd2c4554e915539584285
Etesam Ansari <etesamansari13@gmail.com>
2020-05-13 23:31:42 -0400

    Initial Commit :tada:

---
+++ app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def aboutPage():
+    return "about"
+
+#start the server
+if __name__ == "__main__":
+    app.run()
